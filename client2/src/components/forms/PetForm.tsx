cfed2c22890aef8bda527c8395b9949c51b49a2d
kenzychew <kenzychew@gmail.com>
2025-06-11 00:37:05 +0800

    fixed components and pages with just React import

--- client2/src/components/forms/PetForm.tsx
+++ client2/src/components/forms/PetForm.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { Formik, Form, Field } from 'formik';
 import type { FormikHelpers } from 'formik';
--- client2/src/pages/GroomingPolicyPage.tsx
+++ client2/src/pages/GroomingPolicyPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import PageTransition from '../components/layout/PageTransition';
 
