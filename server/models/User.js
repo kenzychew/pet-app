b159f5e4e6802cadf35be0615940c9b6146666c5
kenzychew <kenzychew@gmail.com>
2025-06-17 00:35:59 +0800

    removed unused code, business hours now managed through appointment model

--- server/models/User.js
+++ server/models/User.js
@@ -9,43 +9,6 @@ const UserSchema = new Schema({
   resetPasswordToken: { type: String },
   resetPasswordExpires: { type: Date },
   createdAt: { type: Date, default: Date.now },
-  workingHours: {
-    type: [
-      {
-        day: {
-          type: String,
-          enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-        },
-        isWorking: { type: Boolean, default: true },
-        startTime: { type: String, default: "09:00" }, // 24h format
-        endTime: { type: String, default: "17:00" },
-      },
-    ],
-    default: [
-      { day: "Monday", isWorking: true, startTime: "09:00", endTime: "17:00" },
-      { day: "Tuesday", isWorking: true, startTime: "09:00", endTime: "17:00" },
-      {
-        day: "Wednesday",
-        isWorking: true,
-        startTime: "09:00",
-        endTime: "17:00",
-      },
-      {
-        day: "Thursday",
-        isWorking: true,
-        startTime: "09:00",
-        endTime: "17:00",
-      },
-      { day: "Friday", isWorking: true, startTime: "09:00", endTime: "17:00" },
-      {
-        day: "Saturday",
-        isWorking: true,
-        startTime: "09:00",
-        endTime: "17:00",
-      },
-      { day: "Sunday", isWorking: true, startTime: "09:00", endTime: "17:00" },
-    ],
-  },
 });
 
 UserSchema.set("toJSON", {
